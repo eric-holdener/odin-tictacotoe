6e1e2b2158bf5bdc89df12c2918bccb619799ce9
EHoldener <eholdener12@gmail.com>
2022-03-18 22:42:02 -0700

    changed tab size to 2

--- ttt.rb
+++ ttt.rb
@@ -2,17 +2,17 @@ require "pry-byebug"
 
 class Game
     @@board_array = Array.new(9)
-    def initialize()
+    def initialize
         @board_array = @@board_array
-        player_x = Player.new("x")
-        player_o = Player.new("o")
+        @player_x = Player.new('x')
+        @player_o = Player.new('o')
     end
 
-    def play_game(board_array)
-        # code
-    end
+  def play_game(board_array)
+    # code
+  end
 
-    def print_board(board_array)
+  def print_board(board_array)
         puts "#{board_array[0]} | #{board_array[1]} | #{board_array[2]}"
         puts "-----"
         puts "#{board_array[3]} | #{board_array[4]} | #{board_array[5]}"
